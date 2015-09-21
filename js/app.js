0388f916fbfc2126e688f04a19d6ae79e516840e
thuongdv_58 <thuongdv_58@vnu.edu.vn>
2015-09-21 17:40:21 +0700

    based version

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 var app = angular.module('quizApp', []);
 app.directive('quiz', function() {
     return {
+<<<<<<< HEAD
+	restrict: 'AE',
+=======
 	restrict: 'E',
+>>>>>>> e9fe9a678c7524795edbcc1f0b178f1ce53dc6f8
 	scope: {},
 	templateUrl: 'questionpanels.html',
 	link: function(scope, elem, attrs) {
@@ -17,4 +21,57 @@ app.directive('quiz', function() {
             };
 	}
     }
+<<<<<<< HEAD
 });
+app.directive ('leaderboard',function(){
+    return {
+        restrict:'AEC',
+        scope:{},
+        templateUrl:'leaderboard.html',
+    }
+});
+app.directive ('profile',function(){
+    return {
+        restrict:'AEC',
+        scope:{},
+        templateUrl:'profile.html',
+    }
+});
+app.controller("globalcontroler",function(){
+    this.option=opt;
+    this.startanswer=function(val)
+    {
+        this.option.quizanswer=true;
+        this.option.quizanswer=val;
+        this.option.menu=false;
+    }
+    this.backtomenu=function()
+    {
+        this.option.menu=true;
+        this.option.leaderboard=false;
+        this.option.profile=false;
+        this.option.answer=false;
+        this.option.login=false;
+    }
+    this.viewleaderboard=function()
+    {
+        this.option.leaderboard=true;
+        this.option.menu=false;
+    }
+    this.editprofile=function()
+    {
+        this.option.profile=true;
+        this.option.menu=false;
+    }
+});
+var option={
+    login:true;
+    menu:false;
+    answer:false;
+    leaderboard:false;
+    profile:false;
+    quizanswer:'none';
+}
+=======
+});
+>>>>>>> e9fe9a678c7524795edbcc1f0b178f1ce53dc6f8
