98f786a9925534a438c31b36fd0043a4145c8ddf
sonnn-58 <sonnn_58@vnu.edu.vn>
2015-10-05 23:02:31 +0700

    fix all

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var app = angular.module('quizApp',['ui.bootstrap']);
 app.run(function($rootScope) {
 	$rootScope.option = "menu";
+	$rootScope.category = "'Sports'";
 	$rootScope.changeTo = function(str){
     	alert("change to "+ str);
     	$rootScope.option = "profile";
-    }
-});
-app.controller('menuController', ['$scope', '$http',
-  function ($scope, $http) {
+    };
+   
+    
+	
+})
+app.controller('menuController', ['$scope', '$http','$attrs',
+  function ($scope, $http, $attrs) {
     $http.get('json/userinfo.json').success(function(xdata) {
         $scope.user = xdata;
     });
@@ -15,10 +19,37 @@ app.controller('menuController', ['$scope', '$http',
 
 app.directive('menuItem',function(){
 	return{
-		restrict: "E",
+		restrict: "EA",
+		controller: ['$scope','$rootScope',function($scope,$rootScope){
+			 $scope.enterStartupScreen = function(str){
+			    	$rootScope.category = str;
+			    	$rootScope.option = "answer";
+			 };
+		}],
 		scope: {
 			item: "="
 		},
 		templateUrl: 'menu-item.html'
 	};
+})
+app.directive("startupScreen",function(){
+	return{
+		restrict: "E",
+		controller: ['$scope','$rootScope','$http',function($scope,$rootScope,$http){
+			$scope.inProcess = false;
+			$scope.backToMenu = function(){
+				$rootScope.option = "menu";
+			}
+			$scope.beginAnswer = function(){
+				$http.get('json/'+category+'question.json').success(function(data) {
+			        $scope.questions = data;
+			    });
+			}
+			
+		}],
+		scope: {
+			category: "="
+		},
+		templateUrl: 'startup-screen.html'
+	}
 })
