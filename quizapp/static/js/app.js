b25fa7991bc061cd6584b05d4a2a24b43b5c6e79
sonnn-58 <sonnn_58@vnu.edu.vn>
2015-10-25 13:14:04 +0700

    complete question and answer

--- quizapp/static/js/app.js
+++ quizapp/static/js/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('quizApp',['ui.bootstrap']);
-
+var WRONG = "danger";
+var RIGHT = "";
 app.run(function($rootScope) {
+	$rootScope.name = "hello";
 	$rootScope.option = "menu";
 	$rootScope.category = "'Sports'";
 	$rootScope.changeTo = function(str){
@@ -8,13 +10,14 @@ app.run(function($rootScope) {
     };
     
 	
-})
-app.controller('menuController', ['$scope', '$http','$attrs',
-  function ($scope, $http, $attrs) {
-    $http.get('/static/json/userinfo.json').success(function(xdata) {
-        $scope.user = xdata;
-    });
-}])
+});
+app.controller('menuController',  ['$scope','$rootScope','$http',function($scope,$rootScope,$http){
+	$scope.user = {
+		name: "hello",
+		kick: "hello",
+		score: 10
+	};
+}]);
 
 app.directive('menuItem',function(){
 	return{
@@ -34,16 +37,74 @@ app.directive('menuItem',function(){
 app.directive("startupScreen",function(){
 	return{
 		restrict: "E",
-		controller: ['$scope','$rootScope','$http',function($scope,$rootScope,$http){
+		controller: ['$scope','$rootScope','$http','$interval',function($scope,$rootScope,$http,$interval){
+			$scope.userScore = 0;
 			$scope.inProcess = false;
+			$scope.isAnswering = false;
+			$scope.userAnswer = "";
+			$scope.addPoint = 0;
 			$scope.backToMenu = function(){
 				$rootScope.option = "menu";
-			}
+			};
 			$scope.beginAnswer = function(){
-				$http.get('static/json/'+category+'question.json').success(function(data) {
+				$scope.end = false;
+				$scope.currentQuestion = 0;
+				$scope.inProcess = true;
+				$scope.isAnswering = true;
+				$scope.addPoint = 0;
+				$scope.results = [];
+				
+				$scope.timer = null;
+				$scope.userAnswers = [];
+				$scope.userAnswers[0] = -1;
+				$http.get('static/json/questionlist.json').success(function(data) {
 			        $scope.questions = data;
 			    });
-			}
+			};
+			var getNumberOfRightAnswer = function(){
+				var count = 0;
+				for (var i = 0; i < $scope.results.length; i++) {
+					if($scope.results[i]!=WRONG) count++;
+				};
+				return count;
+			};
+			var addPointToScore = function(point){
+				$scope.userScore += point;
+			};
+			var nextQuestion = function(){
+				$scope.currentQuestion++;
+				if($scope.currentQuestion>9){
+					$scope.numberOfRightAnswer = getNumberOfRightAnswer();
+					$scope.currentQuestion--;
+					$scope.isAnswering = false;
+					$scope.end = true;
+				}else{
+    			    $scope.inProcess = true;
+					$scope.isAnswering = true;
+					$scope.userAnswers[$scope.currentQuestion] = -1;
+				}
+				$interval.cancel($scope.showResult);
+			};
+			$scope.showDetailResult = function(){
+				$scope.isAnswering = true;
+				$scope.end = true;
+			};
+			$scope.resetProcess = function(){
+				$scope.inProcess = false;
+			};
+			$scope.confirmAnswer = function(){	
+				if($scope.userAnswers[$scope.currentQuestion]<0) return;
+    			if($scope.userAnswers[$scope.currentQuestion]==$scope.questions[$scope.currentQuestion].answer){
+    				$scope.addPoint = 10;
+    				$scope.results[$scope.currentQuestion] = RIGHT;
+    			}else{
+    				$scope.addPoint = 0;
+    				$scope.results[$scope.currentQuestion] = WRONG;
+    			}		
+    			addPointToScore($scope.addPoint);
+    			$scope.isAnswering = false;
+    			$scope.showResult = $interval(nextQuestion,1500);
+			};
 			
 		}],
 		scope: {
@@ -58,7 +119,6 @@ app.directive('leaderboard',function(){
 		controller: ['$scope','$rootScope','$http',function($scope,$rootScope,$http){
 			$http.get('static/json/leaderboard.json').success(function(data) {
 				$scope.players = data;
-				$scope.xsize = 2;
 			});
 			$scope.backToMenu = function(){
 				$rootScope.option = "menu";
