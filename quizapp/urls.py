862c484259fb87e0e3962a702a086a871c1c4342
thuongdv58 <thuongdv_58@vnu.edu.vn>
2015-11-13 19:17:55 +0700

    change the logout views

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.conf import settings
 from . import views
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 # from quizapp.views import MyRegistrationView
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -12,7 +14,9 @@ urlpatterns = [
     url(r'^updatequestionrate', views.update_profile, name='updatequestionrate'),
     url(r'^updatescore', views.update_score, name='updatedcore'),
     url(r'^profile', views.update_profile, name='profile'),
-    url(r'^questionrate', views.update_question_rate, name='questionrate')
+    url(r'^questionrate', views.update_question_rate, name='questionrate'),
+    #url(r'^accounts/register/complete/$',TemplateView.as_view(template_name='quizapp/index.html'),name='registration_complete'),
+    url(r'^accounts/logout/$',auth_views.logout,{'template_name': 'quizapp/index.html'},name='auth_logout'),
     # url(r'^register/$',
     # MyRegistrationView.as_view(),
     #  name='registration_register'),
